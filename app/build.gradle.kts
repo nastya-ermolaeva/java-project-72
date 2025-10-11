37eb85bcce2391ee3f51f22866b10d519aa447df
nastya-ermolaeva <nastyaermolaeva31@mail.ru>
2025-10-11 21:43:32 +0300

    add Javalin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,17 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     java
     id("com.github.ben-manes.versions") version "0.52.0"
     id("checkstyle")
     id("org.sonarqube") version "6.2.0.5505"
     id("jacoco")
+    application
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -14,6 +22,10 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.17")
+    implementation("io.javalin:javalin-rendering:6.7.0")
+    implementation("gg.jte:jte:3.1.9")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -29,6 +41,11 @@ jacoco {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+    }
 }
 
 tasks.jacocoTestReport {
